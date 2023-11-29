11c01d53fe130df87e6d2d0f5d43504b850260f1
Alexander Myronov <alexander.myronov@gmail.com>
2023-11-29 14:01:24 +0100

    ptcr mlm new config

--- bertrand/immrep/training/config.py
+++ bertrand/immrep/training/config.py
@@ -50,3 +50,18 @@ MLM_TRAINING_ARGS = dict(
     save_strategy="epoch",  # model is saved every epoch
     # eval_steps=5,
 )
+
+# Training args for MLM
+MLM_TRAINING_ARGS_PTCR = dict(
+    num_train_epochs=100,  # total number of training epochs
+    per_device_train_batch_size=512+256,  # batch size per device during training
+    per_device_eval_batch_size=512+256,  # batch size for evaluation
+    warmup_ratio=0.01,  # number of warmup steps for learning rate scheduler
+    learning_rate=5e-5,  # learning rate
+    weight_decay=1e-4,  # strength of weight decay
+    logging_dir="./logs",  # directory for storing logs
+    logging_steps=100,  # log every 100 steps
+    evaluation_strategy="epoch",  # model is evaluated every epoch
+    save_strategy="epoch",  # model is saved every epoch
+    # eval_steps=5,
+)
