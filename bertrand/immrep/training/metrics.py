9d55a794e662f71d6b5fa90cd393e84f6341a96e
Alexander Myronov <alexander.myronov@gmail.com>
2023-12-06 12:51:19 +0100

    metric

---
+++ bertrand/immrep/training/metrics.py
@@ -0,0 +1,11 @@
+from sklearn.metrics import roc_auc_score
+
+
+def pAUROC(X_test, probs):
+    roc_pep = X_test.copy().assign(y_pred=probs).groupby('Peptide').apply(
+        lambda x: roc_auc_score(x.y, x.y_pred, max_fpr=0.1))
+    return roc_pep
+
+
+def mean_pAUROC(X_test, probs):
+    return {'mean_pauroc': pAUROC(X_test, probs).mean()}
