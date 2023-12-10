0e8e4459692f5427a88e3dd7d55992364f435479
Alexander Myronov <alexander.myronov@gmail.com>
2023-12-10 23:36:08 +0100

    change

--- bertrand/immrep/training/train_ptcr.py
+++ bertrand/immrep/training/train_ptcr.py
@@ -197,8 +197,8 @@ if __name__ == "__main__":
 
         X = data_split['train_sample'].reset_index(drop=True)
         X_test = data_split['test_sample'].reset_index(drop=True)
-        X_val = X.groupby(['Peptide', 'y']).sample(frac=0.1, random_state=42)
-        X_train = X[~X.index.isin(X_val.index)]
+        X_val = X_test.copy()#X.groupby(['Peptide', 'y']).sample(frac=0.1, random_state=42)
+        X_train = X#X[~X.index.isin(X_val.index)]
 
         # X_train = X_train.sample(4 * 32).reset_index(drop=True)
         # X_val = X_val.sample(2 * 32).reset_index(drop=True)
